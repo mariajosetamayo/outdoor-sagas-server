93916a0fc260ecf4372af5c44a67ffb8428d30f0
Maria Jose Tamayo y Ortiz <mariajose.tamayo@Marias-Mac-mini.local>
2017-02-17 13:00:05 -0700

    added cross origin allow headers

--- index.js
+++ index.js
@@ -12,6 +12,12 @@ const config = require('./config')
 
 mongoose.Promise = global.Promise;
 
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+});
+
 
 // DB Setuo
 
